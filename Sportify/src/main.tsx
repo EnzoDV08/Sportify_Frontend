dd6ba280294162a6c3b7140745463f271fc282b8
JoshuaDeKlerk <231207@virtualwindow.co.za>
2025-05-20 21:22:31 +0200

    Cleaning Up File Structure

--- Sportify/src/main.tsx
+++ Sportify/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Components/App'
+import App from './pages/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <script src="https://cdn.tailwindcss.com/?v=4.1.0"></script>
+    
     <App />
   </React.StrictMode>
 )
--- Sportify/src/pages/App.tsx
+++ Sportify/src/pages/App.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from '../pages/Login'
+import Login from './Login'
 
 const router = createBrowserRouter(
   [
