7639b72219fe0c0ef30e9471daa11d2b2cede064
Enzo De Vittorio <125486388+EnzoDV08@users.noreply.github.com>
2025-06-04 10:13:58 +0200

    Getting the temp hosting ready

--- Sportify/dist-electron/main.js
+++ Sportify/dist-electron/main.js
@@ -1,41 +1,30 @@
-import { app, BrowserWindow } from "electron";
-import { fileURLToPath } from "node:url";
-import path from "node:path";
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-process.env.APP_ROOT = path.join(__dirname, "..");
-const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
-const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
-const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
-process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, "public") : RENDERER_DIST;
-let win;
-function createWindow() {
-  win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+import { app as n, BrowserWindow as t } from "electron";
+import { fileURLToPath as r } from "node:url";
+import o from "node:path";
+const s = o.dirname(r(import.meta.url));
+process.env.APP_ROOT = o.join(s, "..");
+const c = process.env.VITE_DEV_SERVER_URL, R = o.join(process.env.APP_ROOT, "dist-electron"), l = o.join(process.env.APP_ROOT, "dist");
+process.env.VITE_PUBLIC = c ? o.join(process.env.APP_ROOT, "public") : l;
+let e;
+function i() {
+  e = new t({
+    icon: o.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
     webPreferences: {
-      preload: path.join(__dirname, "../dist-electron/preload.js")
+      preload: o.join(s, "../dist-electron/preload.js")
     }
-  });
-  win.maximize();
-  win.webContents.openDevTools();
-  win.webContents.on("did-finish-load", () => {
-    win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
-  });
-  win.loadURL("http://localhost:5173");
+  }), e.maximize(), e.webContents.openDevTools(), e.webContents.on("did-finish-load", () => {
+    e == null || e.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
+  }), e.loadURL("http://localhost:5173");
 }
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-    win = null;
-  }
+n.on("window-all-closed", () => {
+  process.platform !== "darwin" && (n.quit(), e = null);
 });
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+n.on("activate", () => {
+  t.getAllWindows().length === 0 && i();
 });
-app.whenReady().then(createWindow);
+n.whenReady().then(i);
 export {
-  MAIN_DIST,
-  RENDERER_DIST,
-  VITE_DEV_SERVER_URL
+  R as MAIN_DIST,
+  l as RENDERER_DIST,
+  c as VITE_DEV_SERVER_URL
 };
